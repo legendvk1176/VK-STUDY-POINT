7a34bc599adbd0132c0b3fb512365028eaa8d3a4
legendvk1176 <legendvk1176@gmail.com>
2026-09-18 17:55:49 +0530

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-api_id =  28829496
-api_hash  = '8094341dbe5bd50090de8b12a01d04b8'
-bot_token =  '8121411999:AAHj-WZRHzzXrVFcohrAHuKrqVZ85E3GYqk'
-LOG =  -100253268913
+api_id =  21595709
+api_hash  = '6b683b86a90c6fae0fbe50a6494bdd53'
+bot_token =  '8919674457:AAHibFE_lI-ypmj4PMiWSrekJgS_4s6F28k'
+LOG =  -1002661514525
 
